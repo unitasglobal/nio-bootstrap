20c96eec81eaa2843260005758e72bf8a8bfa292
Lewis Franklin <lewis.franklin@unitasglobal.com>
2022-09-12 07:47:15 -0600

    Move pipefail after version check

--- shared_bootstrap.sh
+++ shared_bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eo pipefail
+set -e
 
 # Ensure AWSCLIv2 is installed and accessible
 AWS_VERSION=$(aws --version | awk '{ print $1 }' | awk -F'/' '{ print $NF }')
@@ -14,6 +14,7 @@ fi
 aws configure list >/dev/null 2>&1 || aws sso login
 
 # Setup private PyPI repository
+set -o pipefail
 poetry config repositories.nio "$(aws ssm get-parameter --name '/nio/production/pypi/url' --with-decryption | jq -r '.Parameter.Value')"
 poetry config http-basic.nio \
     "$(aws ssm get-parameter --name '/nio/default/pypi/username' --with-decryption | jq -r '.Parameter.Value')" \
